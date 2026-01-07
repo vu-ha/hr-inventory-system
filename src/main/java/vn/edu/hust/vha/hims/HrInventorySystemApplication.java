4c4f59d68e1cc130788d3306dad344414f46f912
vu-ha <vietha26012005@gmail.com>
2026-01-07 15:16:25 +0700

    chore(app): update application bootstrap config

--- src/main/java/vn/edu/hust/vha/hims/HrInventorySystemApplication.java
+++ src/main/java/vn/edu/hust/vha/hims/HrInventorySystemApplication.java
@@ -2,8 +2,10 @@ package vn.edu.hust.vha.hims;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing 
 public class HrInventorySystemApplication {
 
 	public static void main(String[] args) {
